c5e2d6605186b76738c196b29e9173925b37b6b1
ArmenAvetyan <armennavett@gmail.com>
2025-07-29 15:39:37 +0400

    Deleted unnecessary C files

--- sizeof.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-	printf("Char size: %lu\n", sizeof(char));
-	printf("Int size: %lu\n", sizeof(int));
-	printf("Float size: %lu\n", sizeof(float));
-	printf("Double size: %lu\n", sizeof(double));
-	return 0;
-}
